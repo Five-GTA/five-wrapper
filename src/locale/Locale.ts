66d858da399a8fd18924891a2c087c3290c4e97b
Arthur L <arthurifccl@gmail.com>
2021-02-28 01:39:56 -0300

    test 1.0.5

--- src/locale/Locale.ts
+++ src/locale/Locale.ts
@@ -1,6 +1,6 @@
 export function L(key:string): string {
     const locale = exports["five_locale"];
-    if(!locale || !locale.get){
+    if(!locale){
         return key;
     }
     return locale.get(key);
